c0b7b10b06d39b535c39cdc95c08b77134f81b44
Karl Chvojka <karl.chvojka@protonmail.com>
2023-03-31 01:06:00 +0000

    IMP: Added file and data assets

---
+++ src/globalAssets/data/projects.js
@@ -0,0 +1,56 @@
+/**
+ * Define data for the projects section.
+ */
+const projects = [
+  {
+    title: 'Weather App Rebuild - 2022',
+    image: require('globalAssets/images/projects/weather_rebuild.png'),
+    desc: '2022 Rebuild of the Weather app. An example of a simple API call, and Typescript. App runs within Docker containers, and is Gitlab CI/CD capable.',
+    tech: 'ReactJS, Webpack 5, Babel 7, TypeScript, Docker, Gitlab CI/CD, ESLint, Styled-Components, React Router',
+    git: 'https://github.com/karlchvojka/weather_app_rebuild'
+  },
+  {
+    title: 'Portfolio Rebuild - 2022',
+    image: require('globalAssets/images/projects/portfolio_rebuild_2022.PNG'),
+    desc: 'The 2022 rebuild of my portfolio, using the below ReactJS Template below.',
+    tech: 'ReactJS, Webpack 5, Babel 7, ESLint, Styled-Components, React Router',
+    git: 'https://github.com/karlchvojka/2022_portfolio'
+  },
+  {
+    title: 'ReactJS Template - 2022',
+    image: require('globalAssets/images/projects/react_template_2022.PNG'),
+    desc: 'A CRA Alternative. This was built to better understand how React works, and how to properly configure it.',
+    tech: 'ReactJS, Webpack 5, Babel 7, ESLint, Styled-Components, React Router',
+    git: 'https://github.com/karlchvojka/reactjs_template_2022'
+  },
+  {
+    title: 'Portfolio: 2019',
+    image: require('globalAssets/images/projects/portfolio_rebuild.png'),
+    desc: 'This is a Portfolio rebuild from 2019. Site was updated, code reviewed and updated.',
+    tech: 'ReactJS, Styled-Components, React-Helmet, React Router',
+    git: 'https://github.com/karlchvojka/2019_portfolio'
+  },
+  {
+    title: 'Weather App',
+    image: require('globalAssets/images/projects/weather.png'),
+    desc: 'This project was used specifically for practice passing props, and interacting with a third party API',
+    tech: 'ReactJS',
+    git: 'https://github.com/karlchvojka/weather_app'
+  },
+  {
+    title: 'SQUABBLE: FINAL',
+    image: require('globalAssets/images/projects/squabble.png'),
+    desc: 'Squabble is a budget splitting app. It allows users to view, record, and evenly split budgets while tracking infrequent purchases.',
+    tech: 'ReactJS, Ruby on Rails, Bootstrap 4, React Router',
+    git: 'https://github.com/karlchvojka/lhl-final-project'
+  },
+  {
+    title: 'JUNGLE APP',
+    image: require('globalAssets/images/projects/jungle.png'),
+    desc: 'A mini e-commerce application built with Rails 4.2 for purposes of teaching Rails.',
+    tech: 'Ruby on Rails, Stripe',
+    git: 'https://github.com/karlchvojka/jungle-rails'
+  },
+]
+
+export default projects
